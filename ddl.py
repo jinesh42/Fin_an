0fd64b322a786756b9e5da2452aa9b063339acf2
Jinesh Jain <jain.jinesh220@gmail.com>
2022-08-16 23:08:29 +0530

    load_insert_changes

--- ddl.py
+++ ddl.py
@@ -1,7 +1,8 @@
 import sqlite3 as sq
+import pandas as pd
 con = sq.connect('finance.db')
 cursor = con.cursor()
-create_table_Fin_agg=""" CREATE TABLE IF NOT EXISTS FIN_AGG (
+'''create_table_Fin_agg=""" CREATE TABLE IF NOT EXISTS FIN_AGG (
                         Month integer ,
                         Year integer,
                         Total decimal(18,2),
@@ -9,5 +10,28 @@ create_table_Fin_agg=""" CREATE TABLE IF NOT EXISTS FIN_AGG (
                         Food decimal(18,2),
                         Ex decimal(18,2)
 ); 
-"""  
-cursor.execute(create_table_Fin_agg)
+"""  '''
+create_table_daily_record=""" CREATE TABLE IF NOT EXISTS DAILY_LOGS (
+                          Day integer,
+                          Month integer,
+                          Year integer,
+                           Transport decimal(18,2),
+                        Food decimal(18,2),
+                        Ex decimal(18,2),
+                        Primary key (Day,month,year)
+                          
+);
+"""
+#cursor.execute(create_table_Fin_agg)
+#cursor.execute(create_table_daily_record)
+"""df_agg=pd.read_csv('Agg_records.csv',sep=',')
+cln_df_agg= df_agg.fillna(value=0)
+cln_df_agg.to_sql(name='FIN_AGG',con=con,if_exists='append',index=False)"""
+df_dly=pd.read_csv('Daily_records.csv',sep=',')
+cln_df_dly= df_dly.fillna(value=0)
+cln_df_dly.to_sql(name='DAILY_LOGS',con=con,if_exists='append',index=False)
+#cln_df_agg= df_agg.fillna(value=0)
+#cln_df_agg.to_sql(name='FIN_AGG',con=con,if_exists='append',index=False)
+
+
+con.close()
