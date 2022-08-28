e57a5249f5232edfd209da2808dc50f2208fc7fe
Jinesh Jain <jain.jinesh220@gmail.com>
2022-08-29 00:33:24 +0530

    First_phase_changes_auto

--- ddl.py
+++ ddl.py
@@ -22,16 +22,23 @@ create_table_daily_record=""" CREATE TABLE IF NOT EXISTS DAILY_LOGS (
                           
 );
 """
+#cursor.execute("delete from daily_logs_temp")
 #cursor.execute(create_table_Fin_agg)
 #cursor.execute(create_table_daily_record)
 """df_agg=pd.read_csv('Agg_records.csv',sep=',')
 cln_df_agg= df_agg.fillna(value=0)
 cln_df_agg.to_sql(name='FIN_AGG',con=con,if_exists='append',index=False)"""
+
 df_dly=pd.read_csv('Daily_records.csv',sep=',')
 cln_df_dly= df_dly.fillna(value=0)
-cln_df_dly.to_sql(name='DAILY_LOGS',con=con,if_exists='append',index=False)
+cln_df_dly.to_sql(name='DAILY_LOGS_TEMP',con=con,if_exists='replace',index=False)
 #cln_df_agg= df_agg.fillna(value=0)
 #cln_df_agg.to_sql(name='FIN_AGG',con=con,if_exists='append',index=False)
 
+cursor.execute("insert into daily_logs (Day,Month,Year,Transport,Food,Ex) select * from daily_logs_temp where true on conflict(day,Month,Year) do update set Transport=excluded.Transport,Food=excluded.food,ex=excluded.ex")
+
+cursor.execute("insert into FIN_AGG (Month,Year,Transport,Food,Ex)  select Month,year,sum(Transport) as transport,sum(Food) as food,sum(ex) as ex from daily_logs group by Year,Month on conflict(Month,year) do update set Transport=excluded.Transport,Food=excluded.food,ex=excluded.ex")
+cursor.execute("insert into FIN_AGG (Month,Year,Total)  select Month,year, (Transport+Food+Ex) as Total from (select Month,year,sum(Transport) as transport,sum(Food) as food,sum(ex) as ex from daily_logs group by Year,Month) where true on conflict(Month,year) do update set Total=excluded.Total")
+con.commit()
 
 con.close()
